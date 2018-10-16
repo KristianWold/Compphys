fe38573f5e2fe47b62958952203b3e9ff773d3c2
kriswold <kriswold@student.matnet.uio.no>
2018-10-16 14:24:49 +0200

    added .pdf to gitignore

--- P3/automation.py
+++ P3/automation.py
@@ -1,26 +1,26 @@
 import os
 
-os.system("./twoBody.x 1 1000 50 1")
+os.system("./twoBody.x 1 365.25 50 1")
 os.system("python3 plot.py twoBody Euler 50")
 
-os.system("./twoBody.x 1 1000 100 1")
+os.system("./twoBody.x 1 365.25 100 1")
 os.system("python3 plot.py twoBody Euler 100")
 
-os.system("./twoBody.x 1 1000 200 1")
+os.system("./twoBody.x 1 365.25 200 1")
 os.system("python3 plot.py twoBody Euler 200")
 
-os.system("./twoBody.x 1 1000 400 1")
+os.system("./twoBody.x 1 365.25 400 1")
 os.system("python3 plot.py twoBody Euler 400")
 
 
-os.system("./twoBody.x 2 1000 50 1")
+os.system("./twoBody.x 2 365.25 50 1")
 os.system("python3 plot.py twoBody Verlet 50")
 
-os.system("./twoBody.x 2 1000 100 1")
+os.system("./twoBody.x 2 365.25 100 1")
 os.system("python3 plot.py twoBody Verlet 100")
 
-os.system("./twoBody.x 2 1000 200 1")
+os.system("./twoBody.x 2 365.25 200 1")
 os.system("python3 plot.py twoBody Verlet 200")
 
-os.system("./twoBody.x 2 1000 400 1")
+os.system("./twoBody.x 2 365.25 400 1")
 os.system("python3 plot.py twoBody Verlet 400")
