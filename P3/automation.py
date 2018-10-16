b9b0a1f47a42962ca332c6d5e05084ec83194749
kriswold <kriswold@student.matnet.uio.no>
2018-10-16 16:38:34 +0200

    Cleaned up old programs

--- P3/automation.py
+++ P3/automation.py
@@ -1,5 +1,5 @@
 import os
-
+"""
 os.system("./twoBody.x 1 365.25 50 1")
 os.system("python3 plot.py twoBody Euler 50")
 
@@ -24,3 +24,11 @@ os.system("python3 plot.py twoBody Verlet 200")
 
 os.system("./twoBody.x 2 365.25 400 1")
 os.system("python3 plot.py twoBody Verlet 400")
+"""
+
+
+os.system("./twoBody.x 1 730.5 100 1")
+os.system("python3 plot.py twoBody Euler 100")
+
+os.system("./twoBody.x 2 730.5 100 1")
+os.system("python3 plot.py twoBody Verlet 100")
