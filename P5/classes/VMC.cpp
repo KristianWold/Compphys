844ba9b1daa3023ff7361b7fbd180284dab48c17
KristianWold <kriswold@student.matnat.uio.no>
2018-12-06 00:12:22 +0100

    Added method for optimizing parameters

--- P5/classes/VMC.cpp
+++ P5/classes/VMC.cpp
@@ -1,189 +1,130 @@
-#include<cmath>
-#include<random>
-#include<iostream>
-#include<iomanip>
-#include<chrono>
-#include<armadillo>
-#include<fstream>
+#include <cmath>
+#include <random>
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include <armadillo>
+#include <fstream>
+#include "VMC.hpp"
 
 using namespace std;
 using namespace chrono;
 using namespace arma;
 
-//function pointer type to pass special functons to the class VMC
-typedef double (*myfunc1)(double*, double, mat&, vec&, int);
-typedef double (*myfunc2)(double*, double, mat&);
-
-struct result
+VMC::VMC(int numDim, int numParticles, myfunc1 acceptAmp, myfunc2 localKinetic, myfunc2 localPotential)
 {
-    double E;
-    double Var;
-    double kinetic;
-    double potential;
-    double R12;
-    int accepted;
-};
+    this->numDim = numDim;
+    this->numParticles = numParticles;
+    this->acceptAmp = acceptAmp;
+    this->localKinetic = localKinetic;
+    this->localPotential = localPotential;
 
-class VMC
+    pos = zeros(numDim, numParticles);
+    delta = zeros(numDim);
+}
+
+result VMC::solve(int numCycles, int preCycles, double* params, double omega, bool writeToFile)
 {
-public:
-    myfunc1 acceptAmp;
-    myfunc2 localKinetic, localPotential;
-    int numDim, numParticles, numCycles, accepted;
-    double kineticE, potentialE, k_E, p_E, E, E2, Var, R12, step;
-    mat pos;
-    vec delta;
+    ofstream myfile;
+    step = 2.1/sqrt(params[0]*omega);
+    kineticE = 0;
+    potentialE = 0;
+    k_E = 0;
+    p_E = 0;
+    E = 0;
+    E2 = 0;
+    Var = 0;
+    R12 = 0;
+    accepted = 0;
 
-    mt19937_64 engine;
-    uniform_real_distribution<double> myRandu = uniform_real_distribution<double>(0,1);
-
-    VMC(int numDim, int numParticles, myfunc1 acceptAmp, myfunc2 localKinetic, myfunc2 localPotential)
+    myfile.open("data.txt");
+    for(int i=0; i<numCycles+preCycles; i++)
     {
-        this->numDim = numDim;
-        this->numParticles = numParticles;
-        this->acceptAmp = acceptAmp;
-        this->localKinetic = localKinetic;
-        this->localPotential = localPotential;
-
-        pos = zeros(numDim, numParticles);
-        delta = zeros(numDim);
-    }
-
-    result solve(int numCycles, int preCycles, double* params, double omega)
-    {
-        ofstream myfile;
-        step = 2.1/sqrt(params[0]*omega);
-        kineticE = 0;
-        potentialE = 0;
-        k_E = 0;
-        p_E = 0;
-        E = 0;
-        E2 = 0;
-        Var = 0;
-        R12 = 0;
-        accepted = 0;
-
-        myfile.open("data.txt");
-        for(int i=0; i<numCycles+preCycles; i++)
+        for(int j=0; j<numParticles; j++)
         {
-            for(int j=0; j<numParticles; j++)
+            for(int k=0; k<numDim; k++)
             {
-                for(int k=0; k<numDim; k++)
-                {
-                    delta(k) = (myRandu(engine) - 0.5)*step;
-                }
-                if(myRandu(engine)<acceptAmp(params, omega, pos, delta, j))
-                {
-                    pos.col(j) += delta;
-                    accepted++;
-                }
+                delta(k) = (myRandu(engine) - 0.5)*step;
             }
-            if(i>=preCycles)
+            if(myRandu(engine)<acceptAmp(params, omega, pos, delta, j))
             {
-                kineticE = localKinetic(params, omega, pos);
-                potentialE = localPotential(params, omega, pos);
+                pos.col(j) += delta;
+                accepted++;
+            }
+        }
+        if(i>=preCycles)
+        {
+            kineticE = localKinetic(params, omega, pos);
+            potentialE = localPotential(params, omega, pos);
+            if(writeToFile)
+            {
                 myfile << pos(0,0) << " " << pos(1,0) << " " << pos(2,0) << " "
                        << pos(0,1) << " " << pos(1,1) << " " << pos(2,1) << " "
                        << kineticE + potentialE << "\n";
+            }
 
-                k_E += kineticE;
-                p_E += potentialE;
-                E += kineticE + potentialE;
-                E2 += (kineticE + potentialE)*(kineticE + potentialE);
-                R12 += norm(pos.col(0) - pos.col(1));
+            k_E += kineticE;
+            p_E += potentialE;
+            E += kineticE + potentialE;
+            E2 += (kineticE + potentialE)*(kineticE + potentialE);
+            R12 += norm(pos.col(0) - pos.col(1));
+        }
+    }
+    myfile.close();
+
+    E /= numCycles;
+    E2 /= numCycles;
+    Var = E2 - E*E;
+    k_E /= numCycles;
+    p_E /= numCycles;
+    R12 /= numCycles;
+
+    result myResult = {E, Var, k_E, p_E, R12, accepted};
+    return myResult;
+}
+
+void VMC::optimize(double *params, double range, int step,
+              int maxIter, int numCycles, int preCycles)
+{
+    int paramToChange = 0;  //parameter to increment
+    double energy = 1e10;   //minimum energy, starts as a arbitrary large number
+    double newEnergy = 0;
+    int count = 0;          //how many times the parameters have been
+    double *tempParams = new double[2]; //temporary values for the parameters
+
+    while(count<maxIter)
+    {
+        tempParams[0] = params[0];
+        tempParams[1] = params[1];
+        tempParams[paramToChange] -= range;
+
+        for(int i=-step; i<=step; i++)
+        {
+            //calculate energy for set of parameters
+            newEnergy = this->solve(numCycles, preCycles, tempParams, 1, false).E;
+            if(newEnergy < energy)  //check if new energy is new minimum
+            {
+                //set new minimum and parameters
+                energy = newEnergy;
+                params[paramToChange] = tempParams[paramToChange];
+            }
+            tempParams[paramToChange] += range/step; //increment parameter
+        }
+
+        if(paramToChange == 0)
+        {
+            paramToChange = 1;  //change parameter to be incremented
+        }
+        else
+        {
+            paramToChange = 0;  //swap back to the first parameter
+            count++;
+            range /= 0.5*step;  //and decrese the range to increse resolution
+            if((100*count)%maxIter == 0)
+            {
+                cout << (100*count)/maxIter << endl;
             }
         }
-        myfile.close();
-
-        E /= numCycles;
-        E2 /= numCycles;
-        Var = E2 - E*E;
-        k_E /= numCycles;
-        p_E /= numCycles;
-        R12 /= numCycles;
-
-        result myResult = {E, Var, k_E, p_E, R12, accepted};
-        return myResult;
     }
-};
-
-//----------------------------------------------------------------------------
-inline double acceptAmp1(double* params, double omega, mat &r, vec &delta, int num)
-{
-    double alpha = params[0];
-    double expo = dot(delta, delta+2*r.col(num));
-    return exp(-alpha*omega*expo);
-}
-
-inline double localKinetic1(double* params, double omega, mat &r)
-{
-    double R2 = accu(mat(r%r)); // r%r means elementwise multiplication
-    double alpha = params[0];
-    return -0.5*omega*omega*R2*alpha*alpha + 3*alpha*omega;
-}
-
-inline double localPotential1(double* params, double omega, mat &r)
-{
-    double R2 = accu(mat(r%r)); // r%r means elementwise multiplication
-    return 0.5*omega*omega*R2;
-}
-//----------------------------------------------------------------------------
-
-//----------------------------------------------------------------------------
-inline double acceptAmp2(double* params, double omega, mat &r, vec &delta, int num)
-{
-    int other;
-    if(num == 0) other = 1;
-    else other = 0;
-
-    double alpha = params[0];
-    double beta = params[1];
-
-    double R1 = -alpha*omega*dot(delta,delta+2*r.col(num));
-    double R2 = norm(r.col(num) + delta - r.col(other));
-    double R3 = norm(r.col(num) - r.col(other));
-
-    return exp(R1 + R2/(1+beta*R2) - R3/(1+beta*R3));
-}
-
-inline double localKinetic2(double* params, double omega, mat &r)
-{
-    double alpha = params[0];
-    double beta = params[1];
-
-    double R12 = norm(r.col(0) - r.col(1));
-    double BR = 1 + beta*R12;
-    return localKinetic1(params, omega, r) +
-    1/(2*BR*BR)*(alpha*omega*R12 - 1/(2*BR*BR) - 2/R12 + 2*beta/BR);
-}
-
-inline double localPotential2(double* params, double omega, mat &r)
-{
-    double R2 = accu(mat(r%r)); // r%r means elementwise multiplication
-    return 0.5*omega*omega*R2 + 1/norm(r.col(0) - r.col(1));
-}
-//----------------------------------------------------------------------------
-
-int main(int argc, char const *argv[]) {
-    int numCycles = atoi(argv[1]);
-    int preCycles = atoi(argv[2]);
-    double omega = atof(argv[3]);
-    double alpha = atof(argv[4]);
-    double beta = atof(argv[5]);
-
-    double* params = new double[2];
-    params[0] = alpha;
-    params[1] = beta;
-
-    VMC solver1(3, 2, &acceptAmp2, &localKinetic2, &localPotential2);
-    result myResult = solver1.solve(numCycles, preCycles, params, omega);
-
-    cout << "Energy = " << myResult.E << endl;
-    cout << "Variance = " << myResult.Var << endl;
-    cout << "Ratio = " << myResult.kinetic/myResult.potential << endl;
-    cout << "<|r1 - r2|> = " << myResult.R12 << endl;
-    cout << "Accepted " << myResult.accepted << endl;
-
-
-    return 0;
+    delete tempParams;
 }
---
+++ P5/main.cpp
@@ -0,0 +1,91 @@
+#include <cmath>
+#include <random>
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include <armadillo>
+#include <fstream>
+#include "classes/VMC.hpp"
+
+using namespace std;
+using namespace chrono;
+using namespace arma;
+
+//----------------------------------------------------------------------------
+inline double acceptAmp1(double* params, double omega, mat &r, vec &delta, int num)
+{
+    double alpha = params[0];
+    double expo = dot(delta, delta+2*r.col(num));
+    return exp(-alpha*omega*expo);
+}
+
+inline double localKinetic1(double* params, double omega, mat &r)
+{
+    double R2 = accu(mat(r%r)); // r%r means elementwise multiplication
+    double alpha = params[0];
+    return -0.5*omega*omega*R2*alpha*alpha + 3*alpha*omega;
+}
+
+inline double localPotential1(double* params, double omega, mat &r)
+{
+    double R2 = accu(mat(r%r)); // r%r means elementwise multiplication
+    return 0.5*omega*omega*R2;
+}
+//----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+inline double acceptAmp2(double* params, double omega, mat &r, vec &delta, int num)
+{
+    int other;
+    if(num == 0) other = 1;
+    else other = 0;
+
+    double alpha = params[0];
+    double beta = params[1];
+
+    double R1 = -alpha*omega*dot(delta,delta+2*r.col(num));
+    double R2 = norm(r.col(num) + delta - r.col(other));
+    double R3 = norm(r.col(num) - r.col(other));
+
+    return exp(R1 + R2/(1+beta*R2) - R3/(1+beta*R3));
+}
+
+inline double localKinetic2(double* params, double omega, mat &r)
+{
+    double alpha = params[0];
+    double beta = params[1];
+
+    double R12 = norm(r.col(0) - r.col(1));
+    double BR = 1 + beta*R12;
+    return localKinetic1(params, omega, r) +
+    1/(2*BR*BR)*(alpha*omega*R12 - 1/(2*BR*BR) - 2/R12 + 2*beta/BR);
+}
+
+inline double localPotential2(double* params, double omega, mat &r)
+{
+    double R2 = accu(mat(r%r)); // r%r means elementwise multiplication
+    return 0.5*omega*omega*R2 + 1/norm(r.col(0) - r.col(1));
+}
+//----------------------------------------------------------------------------
+
+int main(int argc, char const *argv[]) {
+    int numCycles = atoi(argv[1]);
+    int preCycles = atoi(argv[2]);
+    double omega  = atof(argv[3]);
+    double alpha  = atof(argv[4]);
+    double beta   = atof(argv[5]);
+
+    double* params = new double[2];
+    params[0] = alpha;
+    params[1] = beta;
+
+    VMC solver(3, 2, &acceptAmp2, &localKinetic2, &localPotential2);
+
+    solver.optimize(params, 0.8, 20, 4, numCycles, preCycles);
+    cout << params[0] << endl;
+    cout << params[1] << endl;
+    result myResult = solver.solve(numCycles, preCycles, params, omega, true);
+    cout << myResult.E << endl;
+
+    return 0;
+}
